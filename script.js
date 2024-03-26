45abee6b7363414eff1ae43d8bf886242a59739a
Bhavani <bhavanipandian1511@gmail.com>
2024-03-26 11:44:56 +0530

    UPDATE. close button is implemented with css and js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const menu = document.querySelector('.menu')
 const menuList = document.querySelector('nav ul')
+const close = document.querySelector('.close')
 menu.addEventListener('click',()=>{
     menuList.classList.toggle('showMenu')
+    menuList.classList.remove('menu')
+    menu.style.display='none'
+    close.style.display='block'
+})
+close.addEventListener('click',()=>{
+    console.log("click")
+    menuList.classList.remove('showMenu')
+    close.style.display='none'
+    menu.style.display='block'
+
 })
